5fdca2da83b60a89f358e46534baf16c395f55a7
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-06-10 14:44:07 +0300

    update signer

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -2,8 +2,6 @@ import { wallet } from 'rgb-connect-nodejs';
 
 const xpub_van = process.env.XPUB_VAN!;
 const xpub_col = process.env.XPUB_COL!;
-const rgbEndpoint = process.env.RGB_MANAGER_ENDPOINT;
-const mnemonic = process.env.MNEMONIC!;
 
 if (!xpub_van) {
   throw new Error('XPUB_VAN is missing from environment variables');
@@ -11,11 +9,8 @@ if (!xpub_van) {
 if (!xpub_col) {
   throw new Error('XPUB_COL is missing from environment variables');
 }
-if (!rgbEndpoint) {
-  throw new Error('RGB_MANAGER_ENDPOINT is missing from environment variables');
-}
 
-wallet.init(xpub_van,xpub_col, rgbEndpoint, mnemonic);
+wallet.init(xpub_van,xpub_col);
 console.log('Wallet initialized with xpub_van:', xpub_van, 'and xpub_col:', xpub_col);
 
 export { wallet };
