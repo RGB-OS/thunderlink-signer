02023dccc46e6ceea77e69ac4ccc92eecff05e17
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-07-16 21:30:44 +0300

    update env

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -4,7 +4,7 @@ const xpub_van = process.env.XPUB_VAN!;
 const xpub_col = process.env.XPUB_COL!;
 const mnemonic = process.env.MNEMONIC;
 const master_fingerprint=  process.env.MASTER_FINGERPRINT!
-const network =  process.env.BITCOIN_NETWORK!
+const network = parseInt(process.env.BITCOIN_NETWORK!, 3);
 
 if(!master_fingerprint){
   throw new Error('MASTER_FINGERPRINT is missing from environment variables');
