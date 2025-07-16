c78abe7dc4a29cdec4adaea7a00d0d4e54f30739
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-07-16 13:04:08 +0300

    latest rgb

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -3,6 +3,12 @@ import { wallet } from 'rgb-connect-nodejs';
 const xpub_van = process.env.XPUB_VAN!;
 const xpub_col = process.env.XPUB_COL!;
 const mnemonic = process.env.MNEMONIC;
+const master_fingerprint=  process.env.MASTER_FINGERPRINT!
+const network =  process.env.BITCOIN_NETWORK!
+
+if(!master_fingerprint){
+  throw new Error('MASTER_FINGERPRINT is missing from environment variables');
+}
 if (!xpub_van) {
   throw new Error('XPUB_VAN is missing from environment variables');
 }
@@ -10,7 +16,7 @@ if (!xpub_col) {
   throw new Error('XPUB_COL is missing from environment variables');
 }
 
-wallet.init({xpub_van,xpub_col,mnemonic});
-console.log('Wallet initialized with xpub_van:', xpub_van, 'and xpub_col:', xpub_col);
+wallet.init({xpub_van,xpub_col,mnemonic,master_fingerprint,network});
+console.log('Wallet initialized with xpub_van:', xpub_van, 'and xpub_col:', xpub_col,'master_fingerprint',master_fingerprint,'network',network);
 
 export { wallet };
